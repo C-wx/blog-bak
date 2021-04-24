21bfafdd54c647384d3195debeaf064a10d648f0
c-wx <1404668149@qq.com>
2021-04-24 23:20:57 +0800

    updte sql&k8sconfig

---
+++ doc/sql/blog.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : vm-mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80024
+ Source Host           : 192.168.108.100:30003
+ Source Schema         : blog
+
+ Target Server Type    : MySQL
+ Target Server Version : 80024
+ File Encoding         : 65001
+
+ Date: 24/04/2021 23:12:33
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
