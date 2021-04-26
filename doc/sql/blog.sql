9ee18b958db29637570bb986c09b93ed23e24f59
wb-cwx733219 <wb-cwx733219@alibaba-inc.com>
2021-04-26 11:56:40 +0800

    update sql

--- doc/sql/blog.sql
+++ doc/sql/blog.sql
@@ -1,21 +1,77 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : vm-mysql
+ Source Server         : 本地数据库
  Source Server Type    : MySQL
- Source Server Version : 80024
- Source Host           : 192.168.108.100:30003
+ Source Server Version : 80020
+ Source Host           : localhost:3306
  Source Schema         : blog
 
  Target Server Type    : MySQL
- Target Server Version : 80024
+ Target Server Version : 80020
  File Encoding         : 65001
 
- Date: 25/04/2021 22:22:48
+ Date: 26/04/2021 11:55:49
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for cb_resource
+-- ----------------------------
+DROP TABLE IF EXISTS `cb_resource`;
+CREATE TABLE `cb_resource`  (
+  `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `resource_icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单图标',
+  `resource_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单名称',
+  `resource_target` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '打开方式',
+  `resource_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单路径',
+  `order` int(0) NULL DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '博客-菜单表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of cb_resource
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cb_role
+-- ----------------------------
+DROP TABLE IF EXISTS `cb_role`;
+CREATE TABLE `cb_role`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT,
+  `parent_id` int(0) NOT NULL DEFAULT 0 COMMENT '父角色ID',
+  `role_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色名称',
+  `role_no` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '角色编号',
+  `desc` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '描述',
+  `order` tinyint(0) NOT NULL DEFAULT 0 COMMENT '排序',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '状态（0:禁用，1:启用）',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `gmt_modify` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '博客-角色表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of cb_role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for cb_role_resource
+-- ----------------------------
+DROP TABLE IF EXISTS `cb_role_resource`;
+CREATE TABLE `cb_role_resource`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `role_id` bigint(0) NULL DEFAULT NULL COMMENT '角色ID',
+  `resource_id` bigint(0) NULL DEFAULT NULL COMMENT '资源ID',
+  `gmt_create` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `gmt_modify` datetime(0) NULL DEFAULT CURRENT_TIMESTAMP(0) ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '博客-角色资源表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of cb_role_resource
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for cb_user
@@ -24,11 +80,11 @@ DROP TABLE IF EXISTS `cb_user`;
 CREATE TABLE `cb_user`  (
   `id` bigint(0) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户名',
-  `user_pwd` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `user_pwd` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
   `user_mobile` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户手机号',
   `user_email` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '邮箱',
   `user_portrait` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '头像',
-  `user_nick_name` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '显示名称',
+  `user_nickname` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '显示名称',
   `user_explain` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '说明',
   `login_last_time` datetime(0) NULL DEFAULT NULL COMMENT '最后登录时间',
   `login_enable` tinyint(1) NULL DEFAULT 1 COMMENT '是否禁用登录（0: 禁用 1：启用）',
@@ -40,5 +96,6 @@ CREATE TABLE `cb_user`  (
 -- ----------------------------
 -- Records of cb_user
 -- ----------------------------
+INSERT INTO `cb_user` VALUES (1, 'admin', '8279603faf7658fded36ce6a400df107', NULL, NULL, NULL, NULL, NULL, NULL, 1, '2021-04-26 10:22:21', '2021-04-26 10:22:21');
 
 SET FOREIGN_KEY_CHECKS = 1;
